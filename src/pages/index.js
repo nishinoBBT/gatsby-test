6c2dbd947f50e8af303590492d7b3657f5e7a2fe
nishino <t_nishino@bigbeat.co.jp>
2019-09-24 17:20:41 +0900

    test-5

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import Layout from '../components/layout'
-
-import Header from '../components/header'
-import Main from '../components/main'
-import Footer from '../components/footer'
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
 
 class IndexPage extends React.Component {
   constructor(props) {
